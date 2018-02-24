c13a4da00ce7ad3a19582d6573dc0582702b390d
raejeong <raychanjeong@gmail.com>
2018-02-24 17:44:46 -0500

    skip

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as functional
 import torch.optim as optim
 from torch.autograd import Variable
+import pdb
 
 class CellDataset(Dataset):
     def __init__(self, X, Y, transform=None):
@@ -26,50 +27,64 @@ class CellDataset(Dataset):
 
         return sample
 
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(1, 5, 9, padding=4)
+        self.pool = nn.MaxPool2d((2,2))
         self.batchnorm1 = nn.BatchNorm2d(6)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.batchnorm2 = nn.BatchNorm2d(16)
-        self.fc1 = nn.Linear(int(3072000/4), 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, 20)
+        self.conv2 = nn.Conv2d(6, 8, 9, padding=4)
+        self.batchnorm2 = nn.BatchNorm2d(14)
+        self.conv3 = nn.Conv2d(14, 20, 9, padding=4)
+        self.batchnorm3 = nn.BatchNorm2d(34)
+        self.fc1 = nn.Linear(int(352512/32), 64)
+        self.fc2 = nn.Linear(64, 20)
+        self.dropout = nn.Dropout3d(p=0.3)
 
     def forward(self, x):
-        x = functional.relu(self.batchnorm1(self.conv1(x)))
-        x = functional.relu(self.batchnorm2(self.conv2(x)))
-        x = x.view(-1, int(3072000/4))
+        x = functional.relu(self.batchnorm1(self.pool(torch.cat((self.conv1(x),x),1))))
+        x = self.dropout(x)
+        x = functional.relu(self.batchnorm2(self.pool(torch.cat((self.conv2(x),x),1))))
+        x = self.dropout(x)
+        x = functional.relu(self.batchnorm3(self.pool(torch.cat((self.conv3(x),x),1))))
+        x = self.dropout(x)
+        x = x.view(-1, int(352512/32))
         x = functional.relu(self.fc1(x))
-        x = functional.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 
-def main():
-    X, Y = np.expand_dims(np.load('X.npy'), 3), np.load('Y.npy')
+def main(hp):
+    # X, Y = np.expand_dims(np.load('X.npy'), 3), np.load('Y.npy')
+    testX, testY = np.expand_dims(np.load('X.npy'), 3), np.load('Y.npy')
+    trainX, trainY = np.load('moreX.npy'), np.load('moreY.npy')
 
-    transform = transforms.Compose([transforms.ToTensor()])
+    transform = transforms.Compose([transforms.ToPILImage(), transforms.RandomCrop(150), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    trainset = CellDataset(X[100:,:,:], Y[100:], transform)
-    trainset_loader = DataLoader(trainset, batch_size=4, shuffle=True)
+    # trainset = CellDataset(X[hp['num_test_images']:,:,:], Y[hp['num_test_images']:], transform)
+    trainset = CellDataset(trainX, trainY, transform)
 
-    testset = CellDataset(X[:100,:,:,:], Y[:100], transform)
-    testset_loader = DataLoader(testset, batch_size=4, shuffle=True)
+    trainset_loader = DataLoader(trainset, batch_size=hp['batch_size'], shuffle=True)
+
+    # testset = CellDataset(X[:hp['num_test_images'],:,:,:], Y[:hp['num_test_images']], transform)
+    testset = CellDataset(testX[:100,:,:,:], testY[:100], transform)
+
+    testset_loader = DataLoader(testset, batch_size=hp['batch_size'], shuffle=True)
 
     net = Net()
+    net.cuda()
+    dtype = torch.cuda.FloatTensor
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(net.parameters(), lr=0.0001)
+    optimizer = optim.Adam(net.parameters(), lr=hp['learning_rate'])
 
-    for epoch in range(30):  # loop over the dataset multiple times
+    best_test_acc = 0
+    for epoch in range(hp['num_epoch']):  # loop over the dataset multiple times
         running_loss = 0.0
         correct = 0
         total = 0
         for i, data in enumerate(trainset_loader):
             inputs, labels = data['image'], data['label']
             # wrap them in Variable
-            inputs, labels = Variable(inputs), Variable(labels)
+            inputs, labels = Variable(inputs).type(dtype), Variable(labels).type(torch.cuda.LongTensor)
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -85,8 +100,8 @@ def main():
 
             # print statistics
             running_loss += loss.data[0]
-            if i % 10 == 9:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 10))
+            if i % hp['print_every'] == hp['print_every']-1:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / hp['print_every']))
                 running_loss = 0.0
         print('Train Accuracy : %d %%' % (100 * correct / total))
         correct = 0
@@ -94,7 +109,7 @@ def main():
         for i, data in enumerate(testset_loader):
             inputs, labels = data['image'], data['label']
             # wrap them in Variable
-            inputs, labels = Variable(inputs), Variable(labels)
+            inputs, labels = Variable(inputs).type(dtype), Variable(labels).type(torch.cuda.LongTensor)
 
             # forward + backward + optimize
             outputs = net(inputs)
@@ -102,10 +117,16 @@ def main():
             total += labels.size(0)
             correct += torch.eq(predicted, labels.data).sum()
 
-        print('Accuracy : %d %%' % (100 * correct / total))
-
-
-
+        best_test_acc = max((100 * correct / total), best_test_acc)
+        print('testAccuracy : %d %%' % (100 * correct / total))
+        print('bestTestAccuracy : %d %%' % best_test_acc)
 
 if __name__ == "__main__":
-    main()
+    hyperparameters = {
+        "num_epoch":50,
+        "batch_size":32,
+        "num_test_images":100,
+        "learning_rate":0.0005,
+        "print_every":10,
+    }
+    main(hyperparameters)
---
+++ Assignment3/more_data.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt 
+
+def imshow(img):
+    plt.imshow(img[:,:,0],cmap='gray')
+    plt.show() 
+
+def main():
+
+    X, Y = np.expand_dims(np.load('X.npy'), 3), np.load('Y.npy')
+    X, Y = X[100:,:,:,:], Y[100:]
+    Xs, Ys = [], []
+    for i in range(20):
+        idx = np.where(Y==i)
+        Xs.append(X[idx])
+        Ys.append(Y[idx])
+    
+    newXs, newYs = [], []
+
+    for i in range(20):
+        newY = Ys[i][0]
+        for j in range(10):
+            newX = np.zeros([150,150]).reshape((150,150,1))
+            for kx in range(3):
+                for ky in range(3):
+                    idx = np.random.randint(Xs[i].shape[0])
+                    currentX = Xs[i][idx,:,:,:]
+                    h, w = currentX.shape[:2]
+                    new_h, new_w = 50, 50
+                    top = np.random.randint(0, h - new_h)
+                    left = np.random.randint(0, w - new_w)
+                    croppedX = currentX[top: top + new_h, left: left + new_w]
+                    newX[kx*50:(kx+1)*50,ky*50:(ky+1)*50,:] = croppedX
+            newXs.append(newX)
+            newYs.append(newY)
+    newXs = np.array(newXs).astype('uint8')
+    newYs = np.array(newYs).astype('int64')
+    np.save("moreX.npy", newXs)
+    np.save("moreY.npy", newYs)
+    print("{} images generated!".format(newYs.shape[0]))
+
+if __name__ == "__main__":
+    main()
